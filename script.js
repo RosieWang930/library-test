850d3a4e11d423ffb0ec86ce377fd32f839d1338
Ruoxi Wang <rw2412@nyu.edu>
2019-11-14 22:40:19 -0500

    first push

---
+++ script.js
@@ -0,0 +1,16 @@
+// var randomColor;
+document.addEventListener('DOMContentLoaded', function(event) {
+
+  var rellax = new Rellax('.rellax');
+
+  ///Try to set ramdom color for icons but failed
+  // randomColor = "rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", " + Math.random() * 255 + ")";
+  // changeColor();
+});
+
+// var iconElement = document.getElementsByClassName("fas")[8];
+//
+// function changeColor(){
+//   iconElement.style.color = 'randomColor';
+//   console.log(iconElement);
+// }
